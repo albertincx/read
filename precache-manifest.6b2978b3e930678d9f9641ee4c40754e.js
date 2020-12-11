178718acde89962c122f327f4f398249e04fea6f
albert <albertincx@gmail.com>
2020-12-11 20:21:57 +0300

    first bliud

---
+++ precache-manifest.6b2978b3e930678d9f9641ee4c40754e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c0f584ffac0772500cd41a418d5ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066cc9f2f25ab1f1982b",
+    "url": "/static/css/2.92a80ae2.chunk.css"
+  },
+  {
+    "revision": "8f8855e6bcb04f522a8e",
+    "url": "/static/css/main.28a40b2c.chunk.css"
+  },
+  {
+    "revision": "066cc9f2f25ab1f1982b",
+    "url": "/static/js/2.60ca8c96.chunk.js"
+  },
+  {
+    "revision": "457f54dd419eef7a4c562e845736f6fc",
+    "url": "/static/js/2.60ca8c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8855e6bcb04f522a8e",
+    "url": "/static/js/main.a54ebd2d.chunk.js"
+  },
+  {
+    "revision": "0d5acb65a23583a3a8b5",
+    "url": "/static/js/runtime-main.e832bbb2.js"
+  }
+]);
