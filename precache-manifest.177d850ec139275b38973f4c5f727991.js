7cbb6408b8ad2a6480c01a6c0163551fe0de25d1
albert <albertincx@gmail.com>
2020-12-12 22:47:23 +0300

    upgrade

---
+++ precache-manifest.177d850ec139275b38973f4c5f727991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd796c84377bbf80d3b220463690ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b05b0b0e035a950995d",
+    "url": "/static/js/2.82a7be60.chunk.js"
+  },
+  {
+    "revision": "2fbaf8de76279fdae52224859715f0cd",
+    "url": "/static/js/2.82a7be60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5301251b33eeb777393b",
+    "url": "/static/js/main.e3b36b15.chunk.js"
+  },
+  {
+    "revision": "446b553bdc131dd121f2",
+    "url": "/static/js/runtime-main.c801d53b.js"
+  }
+]);
